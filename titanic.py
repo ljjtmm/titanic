783da3abfc1fc9bb0f1231154f87fd52c4241d88
ljjtmm <86856014+ljjtmm@users.noreply.github.com>
2023-03-07 19:05:53 +0000

    Load and index data

--- titanic.py
+++ titanic.py
@@ -1,1 +1,9 @@
+#Import relevant packages
 import pandas as pd
+import os
+
+#Load data
+training_data = pd.read_csv('datasets/titanic/train.csv')
+testing_data = pd.read_csv('datasets/titanic/test.csv')
+
+print(training_data.columns)
