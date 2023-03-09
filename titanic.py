c06fd6a2acc31e0ba9516c90fc913e5926b6f1e4
ljjtmm <86856014+ljjtmm@users.noreply.github.com>
2023-03-09 18:30:20 +0000

    Functional approach to RFC

--- titanic.py
+++ titanic.py
@@ -1,10 +1,11 @@
 #Import relevant packages
 import pandas as pd
 import os
-from data_preprocessing import preprocess_pipeline
+from data_preprocessing import preprocessing_pipeline
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.svm import SVC
+from train_models import train_rfc
 
 #Define our files
 training_file = 'datasets/titanic/train.csv'
@@ -14,31 +15,22 @@ testing_file = 'datasets/titanic/test.csv'
 numerical_attribs = ["Age", "SibSp", "Parch", "Fare"]
 categorical_attribs = ["Pclass", "Sex", "Embarked"]
 
+#Define our index column
+index_col = "PassengerId"
+
 #Create our pipeline
-pl = preprocess_pipeline(training_file, testing_file, "PassengerId", numerical_attribs, categorical_attribs)
+pp_func_res = preprocessing_pipeline(training_file, testing_file, index_col, numerical_attribs, categorical_attribs)
 
-
-#Define our training sets
-training_data = pd.read_csv(training_file)
-testing_data = pd.read_csv(testing_file)
+training_data, testing_data, pl= pp_func_res[0], pp_func_res[1], pp_func_res[2]
 
 X_train = pl.fit_transform(training_data[numerical_attribs + categorical_attribs])
 y_train = training_data["Survived"]
 
-#Train a Random Forest classifier
-forest_clf = RandomForestClassifier(n_estimators=100, random_state=42)
-forest_clf.fit(X_train, y_train)
+print(train_rfc(pl, training_file, testing_file, index_col, numerical_attribs, categorical_attribs))
 
-#Create RF predictions
-X_test = pl.transform(testing_data[numerical_attribs + categorical_attribs])
-y_pred = forest_clf.predict(X_test)
 
-#Find the mean of our predictions using cross-validation
-forest_scores = cross_val_score(forest_clf, X_train, y_train, cv=10)
-print("Result of cross-validation for Random Foest model: ",forest_scores.mean()) #Returns 0.8137578027465668
-
-#Test a Support Vector Classifier
-svm_clf = SVC(gamma="auto")
-svm_scores = cross_val_score(svm_clf, X_train, y_train, cv=10)
-print("Mean of Support Vector Classififier model scores :",svm_scores.mean()) #Returns 0.8249313358302123
+# #Test a Support Vector Classifier
+# svm_clf = SVC(gamma="auto")
+# svm_scores = cross_val_score(svm_clf, X_train, y_train, cv=10)
+# print("Mean of Support Vector Classififier model scores :",svm_scores.mean()) #Returns 0.8249313358302123
 
