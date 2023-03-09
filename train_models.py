7d56304e2bc9df2e61596925fccb78a87d4cff8c
ljjtmm <86856014+ljjtmm@users.noreply.github.com>
2023-03-09 18:33:53 +0000

    function to train svc

--- train_models.py
+++ train_models.py
@@ -26,3 +26,18 @@ def train_rfc(pipeline,training_file,testing_file, index_col, numerical_attribs,
     res_string = "Mean of Cross-Validation score for RF Classifer model is: " + str(round(forest_scores.mean(),2))
 
     return res_string
+
+def train_svc(pipeline,training_file,testing_file, index_col, numerical_attribs, categorical_attribs):
+    #Read in result of preprocessing_pipeline function
+    pp_func_res = preprocessing_pipeline(training_file, testing_file, index_col, numerical_attribs, categorical_attribs)
+    training_data, testing_data, pl= pp_func_res[0], pp_func_res[1], pp_func_res[2]
+
+    #Create training set
+    X_train = pl.fit_transform(training_data[numerical_attribs + categorical_attribs])
+    y_train = training_data["Survived"]
+
+    svm_clf = SVC(gamma="auto")
+    svm_scores = cross_val_score(svm_clf, X_train, y_train, cv=10)
+    res_string = "Mean of Support Vector Classififier model scores :" + str(round(svm_scores.mean(), 2)) 
+
+    return res_string
